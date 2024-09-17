90732585d477142e495a8d36f780143462d75b9e
Doan The Khiem <doanthekhiem@gmail.com>
2024-09-17 15:42:22 +0700

    len

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -4,6 +4,12 @@ import React from "react";
 export default function Document() {
   return (
     <Html lang="en">
+      <script
+        src="https://app.10xlaunch.ai/widget"
+        data-app-id="9a1f1e5f-da18-4bd2-8691-45c850955dc0"
+        async
+        defer
+      ></script>
       <body>
         <Main />
         <NextScript />
